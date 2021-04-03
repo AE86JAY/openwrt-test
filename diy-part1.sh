03fa9d693deba924bff3e433e4424a4b57baedef
AE86JAY <AE86JAY@users.noreply.github.com>
2021-04-03 08:50:12 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,7 @@ git clone https://github.com/riverscn/openwrt-iptvhelper.git ./package/diy/iptvh
 # Passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/diy/passwall
 
-# filebrowser
+# 文件管理器
 git clone https://github.com/xiaozhuai/luci-app-filebrowser.git ./package/diy/filebrowser
 
 # App过滤
