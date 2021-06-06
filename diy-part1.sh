45266642688c169c48e32ee95327370639a2e08e
AE86JAY <AE86JAY@users.noreply.github.com>
2021-06-06 12:28:52 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -36,4 +36,6 @@ git clone https://github.com/fw876/helloworld.git ./package/diy/helloworld
 
 # AdGuardHome广告过虑
 git clone https://github.com/AdguardTeam/AdGuardHome.git ./package/diy/AdGuardHome
-https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/diy/luci-app-adguardhome
+
+# # Luci AdGuardHome广告过虑
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/diy/luci-app-adguardhome
